6782f89f7511d311d8a489335e15e1bc0c76baca
Alex Maslakov <me@gildedhonour.com>
2017-11-06 16:02:32 +0800

    wip

--- app/src/main/java/alexmaslakov/me/wifi_old_sport/WiFiAccessPointDbManager.kt
+++ app/src/main/java/alexmaslakov/me/wifi_old_sport/WiFiAccessPointDbManager.kt
@@ -1,24 +1,25 @@
 package alexmaslakov.me.wifi_old_sport
 
-import alexmaslakov.me.wifi_old_sport.WiFiNetworksDbManager.Companion.DATABASE_NAME
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class WiFiAccessPointDbManager(ctx: Context) : SQLiteOpenHelper(ctx, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(DICTIONARY_TABLE_CREATE)
+        db.execSQL(ACCESS_POINTS_TABLE_CREATE)
     }
 
     companion object {
         private val DATABASE_VERSION = 1
-        private val DICTIONARY_TABLE_NAME = "wifi_old_sport"
-        private val DICTIONARY_TABLE_CREATE = "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
-                KEY_WORD + " TEXT, " +
-                KEY_DEFINITION + " TEXT);"
+        private val WIFI_ACCESS_POINTS_TABLE_NAME = "wifi_access_points"
+        private val KEY_WORD = "todo"
+        private val KEY_DEFINITION = "todo"
+
+        private val ACCESS_POINTS_TABLE_CREATE = "CREATE TABLE " + WIFI_ACCESS_POINTS_TABLE_NAME  + " (" + KEY_WORD + " TEXT, " + KEY_DEFINITION + " TEXT);"
+        private val DATABASE_NAME = "wifi_old_sport.db"
     }
 }
